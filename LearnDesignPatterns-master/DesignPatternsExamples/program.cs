272011a8d5f94fb7436f849522ad547be15094ff
snushi <cdnwestie@yahoo.com>
2026-05-11 20:41:36 -0400

    Update comments in program.cs for clarity kn test

--- LearnDesignPatterns-master/DesignPatternsExamples/program.cs
+++ LearnDesignPatterns-master/DesignPatternsExamples/program.cs
@@ -14,7 +14,7 @@ namespace DesignPatternsExamples
         public static void Main(string[] args)
         {
             #region Observer Pattern Example
-//kntest
+//kntest2
             //WeatherData weatherData = new WeatherData(); //Subject
 
             //IObserver currentDisplay = new CurrentConditionDisplay(weatherData); //Observer
